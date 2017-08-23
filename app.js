ecf5d9bf60ffc7205484366f40c6b8339c931cd3
Sathish474 <31286877+Sathish474@users.noreply.github.com>
2017-08-23 14:34:12 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+var mod = angular.module("todoApp", []);
+mod.controller("TodoCtrl", todoCtrl);
+
+function todoCtrl(){
+    this.editMode = false;
+    this.Todos = [        
+            "Learn JavaScript",
+            "Learn AngularJS",
+            "Learn Restful Web Services",
+            "Learn Spring"    
+    ];
+
+    this.addNewTodo = function(){
+        this.Todos.push(this.newTodo);
+        this.newTodo = "";
+    }
+
+this.triggerEditMode = function(){
+    this.editMode = !this.editMode;
+}
+
+this.deleteTodo = function(index){
+    this.Todos.splice(index, 1);
+}
+
+}
+
